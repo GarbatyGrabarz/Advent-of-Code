d85eb09c680ec5135966c6686a84fd1486d005dd
Tomasz <garbatygrabarz@gmail.com>
2021-12-03 22:46:01 +0100

    Add day 3 part 2

--- day3.py
+++ day3.py
@@ -31,4 +31,44 @@ print(f'Gamma rate: {gamma_rate}')
 print(f'Epsilon rate: {epsilon_rate}')
 
 power_consumption = gamma_rate * epsilon_rate
-print(f'The power consumption is: {power_consumption}')
+print(f'The power consumption is: {power_consumption}\n')
+
+# Part 2
+o2 = diagnostic_report.copy()
+co2 = diagnostic_report.copy()
+
+
+def find_bit_value(report, bit_type):
+    for col in report.columns:
+        allbits = len(report)
+        if allbits == 1:
+            break
+        ones = report[col][report[col] == 1].count()
+        zeros = report[col][report[col] == 0].count()
+
+        if bit_type == 'most':
+            if ones >= zeros:
+                bt = 1
+            else:
+                bt = 0
+        elif bit_type == 'least':
+            if ones >= zeros:
+                bt = 0
+            else:
+                bt = 1
+
+        report = report[report[col] == bt]
+
+    binary = ''.join([str(x) for x in report.iloc[0].to_list()])
+    rate = int(binary, 2)
+    return rate
+
+
+o2_gen_rate = find_bit_value(o2, 'most')
+print(f'Oxygen generator rating: {o2_gen_rate}')
+
+co2_scrubb_rate = find_bit_value(co2, 'least')
+print(f'CO2 scrubber rating: {co2_scrubb_rate}')
+
+life_support = o2_gen_rate * co2_scrubb_rate
+print(f'Life support rating: {life_support}')
