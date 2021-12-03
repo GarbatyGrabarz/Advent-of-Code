36d400840dbd62535f65eb0df23fb32eac1413c6
Tomasz Kozlowski <tomasz.kozlowski@mycronic.com>
2021-12-03 11:56:45 +0100

    Add solution to puzzle 1

--- aoc.py
+++ aoc.py
@@ -7,3 +7,7 @@ with open(r'input1.txt', 'r') as file:
 sonar_data = pandas.DataFrame(input, columns=['depth'])
 sonar_data = sonar_data.astype(int)
 del input
+
+# Puzzle 1
+sonar_data['delta'] = sonar_data.depth.diff()
+depth_increase = sonar_data.delta[sonar_data.delta > 0].count()
