3d708870d195c8497c298a7d66fa020c8094128c
Tomasz Kozlowski <tomasz.kozlowski@mycronic.com>
2021-12-03 11:57:01 +0100

    Add solution to puzzle 2

--- aoc.py
+++ aoc.py
@@ -11,3 +11,11 @@ del input
 # Puzzle 1
 sonar_data['delta'] = sonar_data.depth.diff()
 depth_increase = sonar_data.delta[sonar_data.delta > 0].count()
+
+# Puzzle 2
+window = 3
+indexer = pandas.api.indexers.FixedForwardWindowIndexer(window_size=window)
+sonar_data['window_sum'] = sonar_data.depth.rolling(window=indexer).sum()
+
+sonar_data['delta_w'] = sonar_data.window_sum.diff()
+depth_inc_improved = sonar_data.delta_w[sonar_data.delta_w > 0].count()
